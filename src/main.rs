acc143540471e7a2789bf17630f8e84b9992cea6
kaiyes <kaiyes.ansary@gmail.com>
2020-06-13 07:14:50 +0600

    added functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn main() {
+    let number = 3;
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+}
+
