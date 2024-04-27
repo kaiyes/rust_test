2502f70ce50c2551ba0a5a147dc789ce93fc3307
kaiyes <kaiyes.ansary@gmail.com>
2024-04-27 17:17:01 +0600

    int types

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 use std::i8;
 
 fn main() {
-    let _num: i8 = 11; //i32
-    println!("the number is {}", _num);
-    let name: &str = "kaiyes";
-    let middle_name: &str = "ahmad";
-    let sur_name: &str = "ansary";
-    //this prints name
-    /*
-     * multi
-     * line
-     * comments
-     */
-    println!("my name is {} {} {}", name, middle_name, sur_name);
-    let mut _num: i16 = 210; // making it mutable
-    println!("mutated number is: {}", _num);
+    println!("min i8 {}", i8::MIN);
+    println!("max i8 {}", i8::MAX);
+    println!("min i16 {}", i16::MIN);
+    println!("max i16 {}", i16::MAX);
+    println!("min i32 {}", i32::MIN);
+    println!("max i32 {}", i32::MAX);
+    println!("min i64 {}", i64::MIN);
+    println!("max i64 {}", i64::MAX);
+    println!("min isize {}", isize::MIN);
+    println!("max isize {}", isize::MAX);
+    println!("min usize {}", usize::MIN);
+    println!("max usize {}", usize::MAX);
+    println!("min f32 {}", f32::MIN);
+    println!("max f32 {}", f32::MAX);
+    println!("min f64 {}", f64::MIN);
+    println!("max f64 {}", f64::MAX);
 }
