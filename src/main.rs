cad1359dcb260b95e8e6c6588b7ee601cefd3746
kaiyes <kaiyes.ansary@gmail.com>
2024-04-28 00:01:08 +0600

    tuples

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,6 @@ fn main() {
     //array
     let countries = ["Korea", "Japan", "USA"];
     println!("{} {}", countries[1], countries[2]);
+    let mix_tuple: (&str, i8) = ("Malaysia", 1);
+    println!("{} {}", mix_tuple.0, mix_tuple.1);
 }
