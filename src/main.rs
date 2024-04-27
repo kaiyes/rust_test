e539a8415265c21719b97caddb9a090a18213ea9
kaiyes <kaiyes.ansary@gmail.com>
2024-04-27 16:39:57 +0600

    mutable * &str

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,16 @@ use std::i8;
 fn main() {
     let _num: i8 = 11; //i32
     println!("the number is {}", _num);
+    let name: &str = "kaiyes";
+    let middle_name: &str = "ahmad";
+    let sur_name: &str = "ansary";
+    //this prints name
+    /*
+     * multi
+     * line
+     * comments
+     */
+    println!("my name is {} {} {}", name, middle_name, sur_name);
+    let mut _num: i16 = 210; // making it mutable
+    println!("mutated number is: {}", _num);
 }
