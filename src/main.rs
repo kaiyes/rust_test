2d75511e33fb892174c8d74f325f78a502118926
kaiyes <kaiyes.ansary@gmail.com>
2024-04-27 19:32:10 +0600

    loop ?

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,13 @@ fn other() {
     let mut n = 0;
     loop {
         n += 1;
-        println!("{TOKEN_MINT_ADDRESS}");
-        if n >= 10 {
+        if n == 7 {
+            println!("{TOKEN_MINT_ADDRESS}");
+            continue;
+        }
+        if n > 10 {
             break;
         }
+        println!("{n}");
     }
 }
