367ab16413fa8118c94e359cfc66af364a9dfff0
kaiyes <kaiyes.ansary@gmail.com>
2024-04-27 17:21:50 +0600

    multiple variables

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,11 @@
-use std::i8;
-
 fn main() {
-    println!("min i8 {}", i8::MIN);
-    println!("max i8 {}", i8::MAX);
-    println!("min i16 {}", i16::MIN);
-    println!("max i16 {}", i16::MAX);
-    println!("min i32 {}", i32::MIN);
-    println!("max i32 {}", i32::MAX);
-    println!("min i64 {}", i64::MIN);
-    println!("max i64 {}", i64::MAX);
-    println!("min isize {}", isize::MIN);
-    println!("max isize {}", isize::MAX);
-    println!("min usize {}", usize::MIN);
-    println!("max usize {}", usize::MAX);
-    println!("min f32 {}", f32::MIN);
-    println!("max f32 {}", f32::MAX);
-    println!("min f64 {}", f64::MIN);
-    println!("max f64 {}", f64::MAX);
+    //booleans
+    let _is_correct: bool = true;
+
+    //characters
+    let _one_char: char = 'a'; //single quotes not double
+
+    //define multiple variables on multiple lines
+    let (country, capital) = ("Malaysia", "KL");
+    println!("{}: {}", country, capital);
 }
