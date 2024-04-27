09c3869a7c9b5b20d56060e6fdd019fe5423f4ef
kaiyes <kaiyes.ansary@gmail.com>
2024-04-27 19:07:57 +0600

    loop

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,17 @@ const RPC_ADDRESS: &str = "quickNode";
 const TOKEN_MINT_ADDRESS: &str = "whale token mint address";
 
 fn main() {
-    println!("{}", RPC_ADDRESS);
+    println!("{RPC_ADDRESS}");
     other()
 }
 
 fn other() {
-    let x = 15;
-    if x > 10 {
-        println!("{}", TOKEN_MINT_ADDRESS);
-    } else {
-        println!("{}", RPC_ADDRESS)
+    let mut n = 0;
+    loop {
+        n += 1;
+        println!("{TOKEN_MINT_ADDRESS}");
+        if n >= 10 {
+            break;
+        }
     }
 }
