b74570b1c5b22a864290fe0f61552dc6ea375635
kaiyes <kaiyes.ansary@gmail.com>
2024-04-27 18:28:09 +0600

    if else

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
+const RPC_ADDRESS: &str = "quickNode";
+const TOKEN_MINT_ADDRESS: &str = "whale token mint address";
+
 fn main() {
-    //booleans
-    let _is_correct: bool = true;
+    println!("{}", RPC_ADDRESS);
+    other()
+}
 
-    //characters
-    let _one_char: char = 'a'; //single quotes not double
-
-    //define multiple variables on multiple lines
-    let (country, capital) = ("Malaysia", "KL");
-    println!("{}: {}", country, capital);
+fn other() {
+    let x = 15;
+    if x > 10 {
+        println!("{}", TOKEN_MINT_ADDRESS);
+    } else {
+        println!("{}", RPC_ADDRESS)
+    }
 }
