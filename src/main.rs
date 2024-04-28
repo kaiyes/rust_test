185f94bc0f2a62368ac17e28e1dada1bf6f517bc
kaiyes <kaiyes.ansary@gmail.com>
2024-04-28 17:16:40 +0600

    function, basic

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
 fn main() {
-    let number = 13;
+    who(40);
+}
 
-    match number {
-        // Match a single value
-        10 => println!("One!"),
-        // Match several values
-        2 | 3 | 5 | 7 | 11 => println!("This is a prime"),
-        // Match an inclusive range
-        13..=19 => println!("rutgart"),
-        _ => println!("special"),
-    }
+fn who(number: i32) {
+    println!("{} ", number);
 }
