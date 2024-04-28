d1fea07ff22f2b8eab0d1941adbacfb91a4ba7a3
kaiyes <kaiyes.ansary@gmail.com>
2024-04-28 20:56:59 +0600

    fn

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
-    who(40);
+    println!("{} addition result is ", sum_of(1, 3))
 }
 
-fn who(number: i32) {
-    println!("{} ", number);
+fn sum_of(x: i32, y: i32) -> i32 {
+    return x + y;
 }
