3c792c42934ec090fc749dee20b22f20788f2a3d
kaiyes <kaiyes.ansary@gmail.com>
2024-04-29 13:17:33 +0600

    nuff tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,25 @@
-fn main() {
-    println!("{} addition result is ", sum_of(1, 3))
+use anyhow::Result;
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::Read;
+
+#[derive(Serialize, Deserialize, Debug)]
+
+struct Account {
+    public_key: String,
+    private_key: String,
 }
 
-fn sum_of(x: i32, y: i32) -> i32 {
-    return x + y;
+fn main() -> Result<()> {
+    let mut file = File::open("accounts.json")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    let accounts: Vec<Account> = serde_json::from_str(&contents)?;
+
+    for account in accounts {
+        println!("Public Key: {}", account.public_key);
+    }
+
+    Ok(())
 }
